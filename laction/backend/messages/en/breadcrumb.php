5d1304abf724e147d837c381711ecf1c468b38cf
vinodkumarmeda1991 <vinodkumarmeda1991@gmail.com>
2018-01-25 16:11:23 +0530

    commited breadcrubm file

---
+++ laction/backend/messages/en/breadcrumb.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    
+    'common.home' => 'Home',
+    // Settings :: START
+    'settings.page_heading' => 'List Of Roles',
+    'settings.module_name' => 'Settings',
+    'settings.roles' => 'Roles',
+    'settings.permissions' => 'Permissions'
+    // Settings :: END
+];
+
+
+
+
