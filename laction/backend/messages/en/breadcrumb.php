c448f3a9cf294d1ce7b9b9940238f3255cbc1c31
vinodkumarmeda1991 <vinodkumarmeda1991@gmail.com>
2018-01-25 19:45:10 +0530

    messages

--- laction/backend/messages/en/breadcrumb.php
+++ laction/backend/messages/en/breadcrumb.php
@@ -7,7 +7,8 @@ return [
     'settings.roles_heading' => 'List Of Roles',
     'settings.roles' => 'Roles',
     'settings.permissions_heading' => 'List Of Permissions',
-    'settings.permissions' => 'Permissions'
+    'settings.permissions' => 'Permissions',
+    'settings.create_role_permissions' => 'CreateRolePermissions'
     // Settings :: END
 ];
 
