73be81ed11493a7f7f255c96c0047385cc10d9b1
vinodkumarmeda1991 <vinodkumarmeda1991@gmail.com>
2018-01-26 16:57:42 +0530

    layout file changes

--- laction/backend/views/layouts/common/left_side_menu.php
+++ laction/backend/views/layouts/common/left_side_menu.php
@@ -15,34 +15,45 @@
 		<ul class="list-unstyled">
 			<li class="  active"><a
 				href="<?php echo Yii::getAlias('@web').'/dashboard';?>"><i
-					class="zmdi zmdi-view-dashboard"></i> <span class="nav-label">Dashboard</span></a></li>
+					class="fa fa-dashboard"></i> <span class="nav-label">Dashboard</span></a></li>
 
 			<li class=""><a href="<?php echo Yii::getAlias('@web').'/slots';?>"><i
-					class="fa fa-pie-chart"></i> <span class="nav-label">Slots</span></a></li>
+					class="fa fa-calendar" aria-hidden="true"></i><span
+					class="nav-label">Slots</span></a></li>
+
+			<li class=""><a href="#"><i class="fa fa-pie-chart"></i> <span
+					class="nav-label">Reports</span></a></li>
+			<li class="has-submenu"><a href="#"><i class="fa fa-bell"
+					aria-hidden="true"></i><span class="nav-label">Notifications</span><span
+					class="menu-arrow"></span></a>
+				<ul class="list-unstyled">
+					<li class=""><a
+						href="<?php echo Yii::getAlias('@web').'/subjects';?>">Subjects<i
+							class="fa fa-puzzle-piece" aria-hidden="true"></i></a></li>
+					<li><a href="<?php echo Yii::getAlias('@web').'/templates';?>">Templets<i
+							class="fa fa-desktop" aria-hidden="true"></i></a></li>
+
+				</ul></li>
 			<li class=""><a
 				href="<?php echo Yii::getAlias('@web').'/create-user';?>"><i
 					class="fa fa-users"></i> <span class="nav-label">Admin Users</span></a></li>
 
-			<li class=""><a href="#"><i class="fa fa-pie-chart"></i> <span
-					class="nav-label">Reports</span></a></li>
-			<li class="has-submenu"><a href="#"><i class="fa fa-bell"></i><span
-					class="nav-label">Notifications</span><span class="menu-arrow"></span></a>
-				<ul class="list-unstyled">
-					<li class=""><a
-						href="<?php echo Yii::getAlias('@web').'/subjects';?>">Subjects</a></li>
-					<li><a href="<?php echo Yii::getAlias('@web').'/templates';?>">Templets</a></li>
 
-				</ul></li>
 			<li class="has-submenu"><a href="#"><i class="fa fa-cogs"></i> <span
 					class="nav-label">Settings</span><span class="menu-arrow"></span></a>
 				<ul class="list-unstyled">
-					<li><a href="<?php echo Yii::getAlias('@web').'/roles' ?>">Roles</a></li>
-					<li><a href="<?php echo Yii::getAlias('@web').'/permissions' ?>">Permissions</a></li>
+					<li><a href="<?php echo Yii::getAlias('@web').'/roles' ?>">Roles<i
+							class="fa fa-briefcase" aria-hidden="true"></i></a></li>
+					<li><a href="<?php echo Yii::getAlias('@web').'/permissions' ?>">Permissions<i
+							class="fa fa-lock" aria-hidden="true"></i></a></li>
 					<li><a
 						href="<?php echo Yii::getAlias('@web').'/role-permissions';?>">Role
-							Permissions</a></li>
-					<li><a href="<?php echo Yii::getAlias('@web').'/categories';?>">Categories</a></li>
-					<li><a href="<?php echo Yii::getAlias('@web').'/sub-categories';?>">SubCategories</a></li>
+							Permissions<i class="fa fa-key" aria-hidden="true"></i>
+					</a></li>
+					<li><a href="<?php echo Yii::getAlias('@web').'/categories';?>">Categories<i
+							class="fa fa-database" aria-hidden="true"></i></a></li>
+					<li><a href="<?php echo Yii::getAlias('@web').'/sub-categories';?>">SubCategories<i
+							class="fa fa-level-down" aria-hidden="true"></i></a></li>
 
 
 				</ul></li>
