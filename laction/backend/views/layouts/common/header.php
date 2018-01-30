8f939a7ee5c038eacdde417eb077bd564ec9f8d2
vinodkumarmeda1991 <vinodkumarmeda1991@gmail.com>
2018-01-30 23:27:38 +0530

    commit all view files

--- laction/backend/views/layouts/common/header.php
+++ laction/backend/views/layouts/common/header.php
@@ -19,12 +19,16 @@
 			<li class="dropdown text-center"><a data-toggle="dropdown"
 				class="dropdown-toggle" href="#"> <img alt=""
 					src="<?php echo Yii::getAlias('@asset').'/img/avatar-2.jpg'?>"
-					class="img-circle profile-img thumb-sm"> <span class="username">Admin
+					class="img-circle profile-img thumb-sm"> <span class="username"><?php
+    $arrSessionData = Yii::$app->session['session_data'];
+    echo isset($arrSessionData['fullname']) ? $arrSessionData['fullname'] : NULL;
+    ?>
 				</span> <span class="caret"></span>
 			</a>
 				<ul class="dropdown-menu pro-menu fadeInUp animated" tabindex="5003"
 					style="overflow: hidden; outline: none;">
-					<li><a href="#"><i class="fa fa-sign-out"></i> Log Out</a></li>
+					<li><a href="<?php echo Yii::getAlias('@web').'/logout';?>"><i
+							class="fa fa-sign-out"></i> Log Out</a></li>
 				</ul></li>
 			<!-- user login dropdown end -->
 		</ul>
