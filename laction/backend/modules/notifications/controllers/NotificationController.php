bd34a6f2c3bae677c7f959b3f7a5e9e3f069636b
vinodkumarmeda1991 <vinodkumarmeda1991@gmail.com>
2018-01-30 23:28:50 +0530

    commit all notifications files

--- laction/backend/modules/notifications/controllers/NotificationController.php
+++ laction/backend/modules/notifications/controllers/NotificationController.php
@@ -13,6 +13,10 @@ class NotificationController extends Controller
 
     public function beforeAction($action)
     {
+        $objSession = Yii::$app->session;
+        if (! isset($objSession['session_data'])) {
+            $this->redirect(Yii::getAlias('@web') . '/login');
+        }
         $this->enableCsrfValidation = false;
         return parent::beforeAction($action);
     }
--- laction/backend/modules/notifications/views/CreateTemplate.php
+++ laction/backend/modules/notifications/views/CreateTemplate.php
@@ -31,67 +31,115 @@
 							<div class="tab-pane active">
 								<div id="page-wrapper">
 									<div class="container">
-
-										<form name="myForm" class="myForm" action="#" method="POST"
-											enctype="multipart/form-data">
-
+										<form name="template_form" class="myForm" action=""
+											method="post">
 											<div class="panel-body">
 												<div class="row">
 													<div class="col-md-10">
 														<div class="col-md-12">
+															<!-- Message Type :: START -->
 															<div class="form-group">
 																<label class="col-md-4">Message Type</label>
 																<div class="col-md-6">
-																	<select id="ddlPassport" name="dd1"
-																		class="form-control">
-																		<option value="">--Select Type--</option>
-																		<option value="Sms">Sms</option>
-																		<option value="Email">Email</option>
+																	<select id="message_type" name="message_type"
+																		class="form-control"
+																		onchange="enableTemplateSpace(this.value)">
+                                                                        <?php
+                                                                        if (! empty($message_types)) {
+                                                                            foreach ($message_types as $key => $value) {
+                                                                                ?>
+                                                                        <option
+																			value="<?php echo $key; ?>"><?php echo $value; ?></option>        
+                                                                                <?php
+                                                                            }
+                                                                            unset($message_types);
+                                                                        }
+                                                                        ?>
 																	</select>
 																</div>
+																<div id="err_message_type"></div>
 															</div>
+															<!-- Message Type :: END -->
 															<br> <br>
-															<div class="form-group" id="dvsms">
-																<label class="col-md-4">SMS Template</label>
+															<!-- From Email :: START -->
+															<div class="form-group">
+																<label class="col-md-4">From Email</label>
 																<div class="col-md-6">
-																	<textarea name="sms" col="3" rows="4"
-																		class="form-control" placeholder="Type Message..."></textarea>
+																	<input type="text" class="form-control"
+																		name="from_email" id="from_email" value="" />
+																</div>
+																<div id="err_from_email"></div>
+															</div>
+															<!-- From Email :: END -->
+															<br /> <br />
+															<!-- Subject :: START -->
+															<div class="form-group">
+																<label class="col-sm-4 control-label">Subject</label>
+																<div class="col-sm-6">
+																	<select class="form-control" name="senderid_id"
+																		id="senderid_id">
+																		<option value="">--Select Subject--</option>
+																	</select>
+																</div>
+																<div id="err_subject"></div>
+															</div>
+															<!-- Subject :: END -->
+															<br /> <br />
+															<!-- Template Code :: START -->
+															<div class="form-group">
+																<label class="col-md-4">Template Code</label>
+																<div class="col-md-6">
+																	<input type="text" name="code" id="code"
+																		class="form-control" value="" />
+																</div>
+																<div id="err_code"></div>
+															</div>
+															<!-- Template Code :: END -->
+															<br /> <br />
+															<!-- Template Name :: START -->
+															<div class="form-group">
+																<label class="col-md-4">Template Name</label>
+																<div class="col-md-6">
+																	<input type="text" name="name" id="name"
+																		class="form-control" value="" />
+																</div>
+																<div id="err_name"></div>
+															</div>
+															<!-- Template Name :: END -->
+															<!-- SMS Template :: START -->
+															<div class="form-group" id="sms_template">
+																<label class="col-md-4">Template</label>
+																<div class="col-md-6">
+																	<textarea name="template" col="3" rows="4"
+																		id="stemplate" class="form-control"
+																		placeholder="Enter Template Code"></textarea>
 																</div>
 															</div>
+															<!-- SMS Template :: END -->
 															<br> <br>
+															<!-- Email Template :: START -->
 															<div class="form-group grid-container grid-styl">
 																<div class="adjoined-bottom">
 																	<div class="">
-
-
-																		<div class="grid-width-100" name="sendemail">
-
+																		<div class="grid-width-100" name="template">
 																			<div id="editor"></div>
 																		</div>
 																	</div>
 																</div>
 															</div>
-															<div class="form-group">
-																<label class="col-sm-4 control-label">Sender Id</label>
-																<div class="col-sm-6">
-																	<select class="form-control">
-																		<option>--Select Id--</option>
-																		<option>1</option>
-																		<option>2</option>
-																		<option>3</option>
-																		<option>4</option>
-																		<option>5</option>
-																	</select>
-
-																</div>
-															</div>
+															<!-- Email Template :: END -->
 															<br> <br>
+															<!--Template Description :: START -->
 															<div class="form-group">
-																<label class="col-md-4">Code</label>
+																<label class="col-md-4">Description</label>
 																<div class="col-md-6">
-																	<input type="text" name="code" class="form-control">
+																	<textarea name="description" id="description" col="3"
+																		rows="4" class="form-control"
+																		placeholder="Template Description"></textarea>
 																</div>
+																<div id="err_description"></div>
 															</div>
+															<!--Template Description :: END -->
 														</div>
 														<!-- end div -->
 													</div>
@@ -118,21 +166,18 @@
 	</div>
 
 	<script type="text/javascript">
-	initSample();
-   $(function () {
-            $("#ddlPassport").change(function () {
-                if ($(this).val() == "Sms") {
-                    $("#dvsms").show();
-                } else {
-                    $("#dvsms").hide();
-                }
-                if ($(this).val() == "Email"){
-                        $("#cke_editor").show();
-
-                }else{
-                        $("#cke_editor").hide();
-                }
-            });
-        });
-  
+	initSample(); //ckeditor related
+	alert($('#message_type').val());
+		function enableTemplateSpace(message_type){
+			message_type="email";
+			  if(message_type == "sms"){
+				  $("#cke_editor").hide();
+			      $("#sms_template").show();
+				  }else{
+					  $("#sms_template").hide();
+				      $("#cke_editor").show();
+				      	  
+					  }
+			  return true;
+			}
 </script>
